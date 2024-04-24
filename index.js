8db323a4d5c2e6653c5c338e8e78327e51f836d3
Luka <luka.luka.luka93@gmail.com>
2024-04-23 21:02:08 -0700

    Launching the school class

--- index.js
+++ index.js
@@ -18,3 +18,28 @@ const employee1 = new Employee ("Bojana", "construction", 1000);
 const employee2 = new Employee ("Milan", "Architect", 5000);
 employee1.updateInfo(10000)
 console.log(employee1.employeeData());
+
+
+
+class School {
+    constructor (name, role, active) {
+        this.name = name;
+        this.role = role;
+        this.active = active;
+    }
+    schoolData(){
+        return `${this.name} is a ${this.role} and is ${this.active}`;
+    }
+    status(status) {
+        if (this.status === "Active") {
+            return `${active}`;
+        } else if (this.status ==="Inactive") {
+            return `${inactive}`;
+        }
+    }
+}
+
+const professor = new School ("Lora", "Professor", "Active");
+const student = new School("Milan", "Student", "Inactive");
+
+console.log(professor.schoolData())
