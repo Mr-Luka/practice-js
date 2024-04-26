dd8cc9bda94334a8b9963d888026072e73afba7b
Luka <luka.luka.luka93@gmail.com>
2024-04-26 15:35:30 -0700

    setting up the folder

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const person = {
-    name: "Luka",
-    sayHi() {
-        return `Hi my name is ${this.name}`;
-    }
-}
-const jenna = {name: "Jenna"}
-const sayHi = person.sayHi.bind(jenna);
-console.log(sayHi());
+console.log("hi")
